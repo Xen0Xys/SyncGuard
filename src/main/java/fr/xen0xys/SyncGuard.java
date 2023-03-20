a9ab37000430d783bbfc93e464567001f16a5d0b
Czekaj Tom <red57101@gmail.com>
2023-03-20 12:07:33 +0100

    :tada: Initial commit

---
+++ src/main/java/fr/xen0xys/SyncGuard.java
@@ -0,0 +1,13 @@
+package fr.xen0xys;
+
+import java.io.File;
+
+public class SyncGuard {
+
+    public static File sourceFolder;
+    public static File backupFolder;
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/fr/xen0xys/models/FolderInstruction.java
@@ -0,0 +1,13 @@
+package fr.xen0xys.models;
+
+import java.io.File;
+import java.util.List;
+
+public class FolderInstruction extends Thread{
+    private String folderPath;
+    private List<File> files;
+
+    public FolderInstruction(){
+
+    }
+}
