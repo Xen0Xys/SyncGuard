a9148d5288a8be9a79cef0b45ec2271dde7462e3
Czekaj Tom <red57101@gmail.com>
2023-03-20 21:45:39 +0100

    :sparkles: Implement main class with backup system

--- src/main/java/fr/xen0xys/SyncGuard.java
+++ src/main/java/fr/xen0xys/SyncGuard.java
@@ -1,13 +1,32 @@
 package fr.xen0xys;
 
+import fr.xen0xys.models.fs.SGFolder;
+import fr.xen0xys.models.time.TimeParser;
+import fr.xen0xys.systems.BackupSystem;
+
 import java.io.File;
 
 public class SyncGuard {
 
-    public static File sourceFolder;
-    public static File backupFolder;
-
     public static void main(String[] args){
-
+        if(args.length < 2){
+            System.out.println("Usage: java -jar SyncGuard.jar <sourceFolder> <backupFolder> <duration|s m h d w>");
+            System.exit(1);
+        }
+        SGFolder sourceFolder = new SGFolder(new File(args[0]));
+        SGFolder backupFolder = new SGFolder(new File(args[1]));
+        long duration = TimeParser.parse(args[2]);
+        BackupSystem backupSystem = new BackupSystem(backupFolder, sourceFolder);
+        while(true){
+            System.out.println("##########################################");
+            System.out.println("#                Syncing...              #");
+            System.out.println("##########################################");
+            backupSystem.sync();
+            try {
+                Thread.sleep(duration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
