9de28d42cdcb772652323be3f2b97a8ee47a6200
Czekaj Tom <red57101@gmail.com>
2023-03-21 09:28:11 +0100

    :rocket: Improve main readability and logging

--- src/main/java/fr/xen0xys/SyncGuard.java
+++ src/main/java/fr/xen0xys/SyncGuard.java
@@ -9,14 +9,27 @@ import java.io.File;
 public class SyncGuard {
 
     public static void main(String[] args){
-        if(args.length < 2){
+        if(args.length < 3){
             System.out.println("Usage: java -jar SyncGuard.jar <sourceFolder> <backupFolder> <duration|s m h d w>");
             System.exit(1);
         }
         SGFolder sourceFolder = new SGFolder(new File(args[0]));
         SGFolder backupFolder = new SGFolder(new File(args[1]));
-        long duration = TimeParser.parse(args[2]);
-        BackupSystem backupSystem = new BackupSystem(backupFolder, sourceFolder);
+        try{
+            long duration = TimeParser.parse(args[2]);
+            BackupSystem backupSystem = new BackupSystem(backupFolder, sourceFolder);
+            run(backupSystem, duration);
+        } catch (NumberFormatException e){
+            System.out.println("Invalid duration format");
+            System.exit(1);
+        } catch (IllegalArgumentException e){
+            System.out.println("Invalid time unit");
+            System.exit(1);
+        }
+    }
+
+    @SuppressWarnings({"InfiniteLoopStatement", "BusyWait"})
+    private static void run(BackupSystem backupSystem, long duration){
         while(true){
             System.out.println("##########################################");
             System.out.println("#                Syncing...              #");
